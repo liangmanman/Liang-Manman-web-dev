73ca62d8804fb68581cee2860898e5960be75050
manmanliang <liangmanman1@LIANGMANMANs-MacBook-Air.local>
2016-06-12 23:17:12 -0400

    openshift fix

--- assignment/models/models.js
+++ assignment/models/models.js
@@ -2,8 +2,17 @@
  * Created by liangmanman1 on 6/6/16.
  */
 module.exports = function() {
+
+    var connectionString = 'mongodb://127.0.0.1:27017/cs4550summer1';
+    if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+        connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+            process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+            process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+            process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+            process.env.OPENSHIFT_APP_NAME;
+    }
     var mongoose = require('mongoose');
-    mongoose.connect('mongodb://127.0.0.1:27017/cs4550summer1');
+    mongoose.connect(connectionString);
 
     var userModel = require("./user/user.model.server.js")();
     var websiteModel = require("./website/website.model.server.js")();
